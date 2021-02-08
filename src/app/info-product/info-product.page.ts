5fca53ed3ca1f10ff4624b058254bccff5869f3f
BusinessCat-ta <stefano.cattarin@stud.tecnicosuperiorekennedy.it>
2021-02-08 15:02:31 +0100

    Remake Magazzino

--- src/app/info-product/info-product.page.ts
+++ src/app/info-product/info-product.page.ts
@@ -1,4 +1,7 @@
-import { MenuController } from '@ionic/angular';
+import { ProdDetailComponent } from './prod-detail/prod-detail.component';
+import { m_product } from './../../models/Magazzino';
+import { ApiServiceService } from './../api-service.service';
+import { MenuController, ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,14 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoProductPage implements OnInit {
 
-  constructor(private router: Router, private menuCtrl: MenuController) { }
+  list: m_product;
+
+  constructor(private router: Router, private menuCtrl: MenuController, private Api: ApiServiceService, private ModalCtrl: ModalController) { }
 
   ngOnInit() {
     this.menuCtrl.enable (true, 'InfoProduct');
+    this.getList();
   }
 
   Direct(route: string){
     this.router.navigateByUrl(route);
+  }
+
+  getList(){
+    this.Api.getProdList("").subscribe((data)=>{
+      this.list= data;
+      console.log(this.list);
+    });
+  }
+
+  async showModal(prod: m_product) {
+    const modal = await this.ModalCtrl.create({
+      component: ProdDetailComponent,
+      componentProps: {
+        Prod: prod
+      }
+    })
+    await modal.present();
+  }
+
+  onSearchChange(ev: any){
+    this.Api.getProdList(ev.detail.value).subscribe((data)=>{
+      this.list = data;
+    })
   }
   
 
